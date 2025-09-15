d54b60c53fdf0c54ef4192502de8a63175a36d5e
Anya-wUw <mykittyistoocute@mail.ru>
2025-09-15 14:37:33 +0300

    delete previous project

--- src/evals/popqa.py
+++
@@ -1,9 +0,0 @@
-from evals.base import Evaluator
-
-# class TOFUEvaluator(Evaluator):
-#     def __init__(self, eval_cfg, **kwargs):
-#         super().__init__("TOFU", eval_cfg, **kwargs)
-
-class PopQAEvaluator(Evaluator):
-    def __init__(self, eval_cfg, **kwargs):
-        super().__init__("PopQA", eval_cfg, **kwargs)
